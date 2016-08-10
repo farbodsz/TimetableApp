c47f1ce1ed8d759bec19e07fe231deeb65f49587
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-10 15:28:13 +0100

    Initial commit

---
+++ app/src/main/java/com/satsumasoftware/timetable/BaseActivity.java
@@ -0,0 +1,197 @@
+package com.satsumasoftware.timetable;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    /**
+     * This method should be overridden in subclasses of BaseActivity to use the Toolbar
+     * Return null if there is no Toolbar
+     */
+    protected Toolbar getSelfToolbar() {
+        return null;
+    }
+
+    /**
+     * This method should be overridden in subclasses of BaseActivity to use the DrawerLayout
+     * Return null if there is no DrawerLayout
+     */
+    protected DrawerLayout getSelfDrawerLayout() {
+        return null;
+    }
+
+    /**
+     * Returns the navigation drawer item that corresponds to this Activity. Subclasses
+     * of BaseActivity override this to indicate what nav drawer item corresponds to them
+     * Return NAVDRAWER_ITEM_INVALID to mean that this Activity should not have a Nav Drawer.
+     */
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_INVALID;
+    }
+
+    /**
+     * Returns the NavigationView that corresponds to this Activity. Subclasses
+     * of BaseActivity override this to indicate what nav drawer item corresponds to them
+     * Return null to mean that this Activity should not have a Nav Drawer.
+     */
+    protected NavigationView getSelfNavigationView() {
+        return null;
+    }
+
+    /*
+     * This is a list of all items in the nav drawer and their corresponding menu ids
+     */
+    protected static final int NAVDRAWER_ITEM_HOME = R.id.navigation_item_home;
+    protected static final int NAVDRAWER_ITEM_INVALID = -1;
+
+    /*
+     * Variables for BaseActivity
+     */
+    private static final int NAVDRAWER_LAUNCH_DELAY = 250;
+    private static final int MAIN_CONTENT_FADEIN_DURATION = 250;
+    private static final int MAIN_CONTENT_FADEOUT_DURATION = 150;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
+    private View mMainContent;
+
+
+    /*
+     * Methods
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        mDrawerLayout = getSelfDrawerLayout();
+        if (mDrawerLayout == null) {
+            return;
+        }
+
+        // gets the main content view in the DrawerLayout
+        mMainContent = mDrawerLayout.getChildAt(0);
+
+        mMainContent.setAlpha(0);
+        mMainContent.animate()
+                .alpha(1)
+                .setDuration(MAIN_CONTENT_FADEIN_DURATION);
+
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        Toolbar toolbar = getSelfToolbar();
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        mNavigationView = getSelfNavigationView();
+        if (mNavigationView == null) {
+            return;
+        }
+        mNavigationView.getMenu().findItem(getSelfNavDrawerItem()).setChecked(true);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                handleNavigationSelection(menuItem);
+                return true;
+            }
+        });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+    }
+
+    private void handleNavigationSelection(final MenuItem menuItem) {
+        if (menuItem.getItemId() == getSelfNavDrawerItem()) {
+            mDrawerLayout.closeDrawers();
+            return;
+        }
+
+        // launch the target Activity after a short delay, to allow the close animation to play
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goToNavDrawerItem(menuItem.getItemId());
+            }
+        }, NAVDRAWER_LAUNCH_DELAY);
+
+        if (menuItem.isCheckable()) {
+            mNavigationView.getMenu().findItem(getSelfNavDrawerItem()).setChecked(false);
+            menuItem.setChecked(true);
+
+            mMainContent.animate()
+                    .alpha(0)
+                    .setDuration(MAIN_CONTENT_FADEOUT_DURATION);
+        }
+
+        mDrawerLayout.closeDrawers();
+    }
+
+    private void goToNavDrawerItem(int menuItem) {
+        Intent intent;
+        switch (menuItem) {
+            case NAVDRAWER_ITEM_HOME:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // pass any configuration change to the drawer toggle
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/satsumasoftware/timetable/MainActivity.java
@@ -0,0 +1,40 @@
+package com.satsumasoftware.timetable;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected Toolbar getSelfToolbar() {
+        return (Toolbar) findViewById(R.id.toolbar);
+    }
+
+    @Override
+    protected DrawerLayout getSelfDrawerLayout() {
+        return (DrawerLayout) findViewById(R.id.drawerLayout);
+    }
+
+    @Override
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_HOME;
+    }
+
+    @Override
+    protected NavigationView getSelfNavigationView() {
+        return (NavigationView) findViewById(R.id.navigationView);
+    }
+
+}
