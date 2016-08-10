a8e2dd4189695c6fe2c43a5ebcc555803c305daf
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-10 22:58:30 +0100

    Invoke SubjectDetailActivity by clicking the FAB

--- app/src/main/java/com/satsumasoftware/timetable/ui/SubjectsActivity.java
+++ app/src/main/java/com/satsumasoftware/timetable/ui/SubjectsActivity.java
@@ -1,5 +1,7 @@
 package com.satsumasoftware.timetable.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -7,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.satsumasoftware.timetable.R;
 import com.satsumasoftware.timetable.framework.Subject;
@@ -16,7 +19,10 @@ import java.util.ArrayList;
 
 public class SubjectsActivity extends BaseActivity {
 
+    protected static final int REQUEST_CODE_SUBJECT_DETAIL = 1;
+
     private ArrayList<Subject> mSubjects;
+    private SubjectsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +35,34 @@ public class SubjectsActivity extends BaseActivity {
         // TODO get subjects from a database
         mSubjects = new ArrayList<>();
 
+        mAdapter = new SubjectsAdapter(mSubjects);
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(new SubjectsAdapter(mSubjects));
+        recyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubjectsActivity.this, SubjectDetailActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SUBJECT_DETAIL);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SUBJECT_DETAIL) {
+            if (resultCode == Activity.RESULT_OK) {
+                Subject modifiedSubject = data.getParcelableExtra(SubjectDetailActivity.EXTRA_SUBJECT);
+                mSubjects.add(modifiedSubject);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
     }
 
     @Override
