f24eb76e5a94d7355af32d56c189a25357ea696f
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-10 22:28:33 +0100

    Make Subject implement Parcelable

--- app/src/main/java/com/satsumasoftware/timetable/framework/Subject.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Subject.kt
@@ -1,3 +1,24 @@
 package com.satsumasoftware.timetable.framework
 
-class Subject(val id: Int, var name: String)
+import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
+
+class Subject(val id: Int, var name: String) : Parcelable {
+
+    constructor(source: Parcel): this(source.readInt(), source.readString())
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id)
+        dest?.writeString(name)
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Subject> = object : Parcelable.Creator<Subject> {
+            override fun createFromParcel(source: Parcel): Subject = Subject(source)
+            override fun newArray(size: Int): Array<Subject?> = arrayOfNulls(size)
+        }
+    }
+}
