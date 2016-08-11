67cf708184b825e3ef1c2cbce06f49ab697c0284
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-11 16:39:58 +0100

    Create a constructor for Class.kt taking Cursor as the argument

--- app/src/main/java/com/satsumasoftware/timetable/framework/Class.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Class.kt
@@ -1,7 +1,24 @@
 package com.satsumasoftware.timetable.framework
 
+import android.database.Cursor
+import com.satsumasoftware.timetable.db.ClassesSchema
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalTime
 
 class Class(val id: Int, val subjectId: Int, val day: DayOfWeek, val startTime: LocalTime,
-            val endTime: LocalTime, val room: String, val teacher: String)
+            val endTime: LocalTime, val room: String, val teacher: String) {
+
+    constructor(cursor: Cursor) : this(
+            cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_ID)),
+            cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_SUBJECT_ID)),
+            DayOfWeek.of(cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_DAY))),
+            LocalTime.of(
+                    cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_START_TIME_HRS)),
+                    cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_START_TIME_MINS))),
+            LocalTime.of(
+                    cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_END_TIME_HRS)),
+                    cursor.getInt(cursor.getColumnIndex(ClassesSchema.COL_END_TIME_MINS))),
+            cursor.getString(cursor.getColumnIndex(ClassesSchema.COL_ROOM)),
+            cursor.getString(cursor.getColumnIndex(ClassesSchema.COL_TEACHER)))
+
+}
