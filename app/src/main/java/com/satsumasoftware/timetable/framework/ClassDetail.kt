704e480400d2b89f0406777c3e36391aac98e37e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-11 23:02:39 +0100

    Remove redundant Parcelable implementation in ClassDetail.kt

--- app/src/main/java/com/satsumasoftware/timetable/framework/ClassDetail.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/ClassDetail.kt
@@ -1,32 +1,6 @@
 package com.satsumasoftware.timetable.framework
 
-import android.os.Parcel
-import android.os.Parcelable
-
 import java.util.*
 
 class ClassDetail(val id: Int, val room: String, val teacher: String,
-                  val classTimeIds: ArrayList<Int>) : Parcelable {
-
-    constructor(source: Parcel) : this(
-            source.readInt(),
-            source.readString(),
-            source.readString(),
-            ArrayList<Int>().apply{ source.readList(this, Int::class.java.classLoader) })
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-        dest?.writeInt(id)
-        dest?.writeString(room)
-        dest?.writeString(teacher)
-        dest?.writeList(classTimeIds)
-    }
-
-    companion object {
-        @JvmField val CREATOR: Parcelable.Creator<ClassDetail> = object : Parcelable.Creator<ClassDetail> {
-            override fun createFromParcel(source: Parcel): ClassDetail = ClassDetail(source)
-            override fun newArray(size: Int): Array<ClassDetail?> = arrayOfNulls(size)
-        }
-    }
-}
+                  val classTimeIds: ArrayList<Int>)
