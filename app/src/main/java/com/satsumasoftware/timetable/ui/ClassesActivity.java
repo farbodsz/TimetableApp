96e34a5cf247d8b6b30ae483c25665b690116e79
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-11 16:52:48 +0100

    Display classes using the values from the database

--- app/src/main/java/com/satsumasoftware/timetable/ui/ClassesActivity.java
+++ app/src/main/java/com/satsumasoftware/timetable/ui/ClassesActivity.java
@@ -1,14 +1,28 @@
 package com.satsumasoftware.timetable.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
+import android.view.View;
 
 import com.satsumasoftware.timetable.R;
+import com.satsumasoftware.timetable.db.ClassesSchema;
+import com.satsumasoftware.timetable.db.DatabaseUtils;
+import com.satsumasoftware.timetable.db.TimetableDbHelper;
+import com.satsumasoftware.timetable.framework.Class;
+import com.satsumasoftware.timetable.framework.ClassGroup;
+import com.satsumasoftware.timetable.ui.adapter.ClassesAdapter;
 
 public class ClassesActivity extends BaseActivity {
+
+    private SparseArray<ClassGroup> mClasses;
+    private ClassesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +32,38 @@ public class ClassesActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mClasses = new SparseArray<>(DatabaseUtils.getHighestSubjectId(this));
+        TimetableDbHelper dbHelper = TimetableDbHelper.getInstance(this);
+        Cursor cursor = dbHelper.getReadableDatabase().query(
+                ClassesSchema.TABLE_NAME, null, null, null, null, null, null);
+        while (!cursor.isAfterLast()) {
+            Class cls = new Class(cursor);
+            int subjectId = cls.getSubjectId();
+            ClassGroup classGroup = mClasses.get(subjectId, new ClassGroup(subjectId));
+            classGroup.addClass(cls);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        mAdapter = new ClassesAdapter(this, mClasses);
+        mAdapter.setOnEntryClickListener(new ClassesAdapter.OnEntryClickListener() {
+            @Override
+            public void onEntryClick(View view, int position) {
+                // TODO
+            }
+        });
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
     }
 
     @Override
