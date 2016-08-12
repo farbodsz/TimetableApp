ea8e2f3aa3fd4648ac2c9b955fbad75278aa5350
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-12 17:49:40 +0100

    Make ClassTime implement Parcelable

--- app/src/main/java/com/satsumasoftware/timetable/framework/ClassTime.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/ClassTime.kt
@@ -1,11 +1,15 @@
 package com.satsumasoftware.timetable.framework
 
+import java.util.*
+import android.os.Parcel
+import android.os.Parcelable
+
 import android.database.Cursor
 import com.satsumasoftware.timetable.db.ClassTimesSchema
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalTime
 
-class ClassTime(val id: Int, val day: DayOfWeek, val startTime: LocalTime, val endTime: LocalTime) {
+class ClassTime(val id: Int, val day: DayOfWeek, val startTime: LocalTime, val endTime: LocalTime) : Parcelable {
 
     constructor(cursor: Cursor) : this(
             cursor.getInt(cursor.getColumnIndex(ClassTimesSchema._ID)),
@@ -17,4 +21,25 @@ class ClassTime(val id: Int, val day: DayOfWeek, val startTime: LocalTime, val e
                     cursor.getInt(cursor.getColumnIndex(ClassTimesSchema.COL_END_TIME_HRS)),
                     cursor.getInt(cursor.getColumnIndex(ClassTimesSchema.COL_END_TIME_MINS))))
 
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readSerializable() as DayOfWeek,
+            source.readSerializable() as LocalTime,
+            source.readSerializable() as LocalTime)
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id)
+        dest?.writeSerializable(day)
+        dest?.writeSerializable(startTime)
+        dest?.writeSerializable(endTime)
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<ClassTime> = object : Parcelable.Creator<ClassTime> {
+            override fun createFromParcel(source: Parcel): ClassTime = ClassTime(source)
+            override fun newArray(size: Int): Array<ClassTime?> = arrayOfNulls(size)
+        }
+    }
 }
