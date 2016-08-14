2ec4b3a2008549f37677b5cd41229be3a02734bb
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-15 00:08:19 +0100

    Make a Color class to store data about colors

---
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Color.kt
@@ -0,0 +1,47 @@
+package com.satsumasoftware.timetable.framework
+
+import android.content.Context
+import android.content.res.Resources
+import android.support.annotation.ColorRes
+
+class Color(val id: Int) {
+
+    val name: String
+        get() = when(id) {
+            1 -> "red"
+            2 -> "pink"
+            3 -> "purple"
+            4 -> "deep_purple"
+            5 -> "indigo"
+            6 -> "blue"
+            7 -> "light_blue"
+            8 -> "cyan"
+            9 -> "teal"
+            10 -> "green"
+            11 -> "light_green"
+            12 -> "lime"
+            13 -> "yellow"
+            14 -> "amber"
+            15 -> "orange"
+            16 -> "deep_orange"
+            17 -> "brown"
+            18 -> "blue_grey"
+            19 -> "brown"
+            else -> throw IllegalArgumentException("invalid color id '$id'")
+        }
+
+    @ColorRes
+    fun getPrimaryColorResId(context: Context) = getColor(context, "mdu_${name}_500")
+
+    @ColorRes
+    fun getPrimaryDarkColorResId(context: Context) = getColor(context, "mdu_${name}_700")
+
+    @ColorRes
+    private fun getColor(context: Context, resName: String) = try {
+        context.resources.getIdentifier(resName, "color", context.packageName)
+    } catch (e: Resources.NotFoundException) {
+        e.printStackTrace()
+        0
+    }
+
+}
