5ab018a61bd5672b3b1c79be1d300f8d9a3367cf
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-15 17:06:31 +0100

    Add an extra column to reference another table's _id column

--- app/src/main/java/com/satsumasoftware/timetable/db/ClassDetailsSchema.java
+++ app/src/main/java/com/satsumasoftware/timetable/db/ClassDetailsSchema.java
@@ -5,11 +5,13 @@ import android.provider.BaseColumns;
 public final class ClassDetailsSchema implements BaseColumns {
 
     public static final String TABLE_NAME = "class_details";
+    public static final String COL_CLASS_ID = "class_id";
     public static final String COL_ROOM = "room";
     public static final String COL_TEACHER = "teacher";
 
     protected static final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + "( " +
             _ID + SchemaUtilsKt.INTEGER_TYPE + SchemaUtilsKt.PRIMARY_KEY_AUTOINCREMENT + SchemaUtilsKt.COMMA_SEP +
+            COL_CLASS_ID + SchemaUtilsKt.INTEGER_TYPE + SchemaUtilsKt.COMMA_SEP +
             COL_ROOM + SchemaUtilsKt.INTEGER_TYPE + SchemaUtilsKt.COMMA_SEP +
             COL_TEACHER + SchemaUtilsKt.INTEGER_TYPE +
             " )";
