cd47cca5f12152777fc03431d291a27015c60e17
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-15 19:29:07 +0100

    Add a property for recording completion progress of assignments

--- app/src/main/java/com/satsumasoftware/timetable/framework/Assignment.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Assignment.kt
@@ -3,4 +3,4 @@ package com.satsumasoftware.timetable.framework
 import org.threeten.bp.LocalDate
 
 class Assignment(val id: Int, val classId: Int, val title: String, val detail: String,
-                 val dueDate: LocalDate)
+                 val dueDate: LocalDate, val completionProgress: Int)
