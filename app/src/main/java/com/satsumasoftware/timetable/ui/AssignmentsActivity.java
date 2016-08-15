c4f66a1764a1225dc619d160229175acb24cfe45
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-15 21:44:19 +0100

    Add Intents to AssignmentDetailActivity from AssignmentsActivity

--- app/src/main/java/com/satsumasoftware/timetable/ui/AssignmentsActivity.java
+++ app/src/main/java/com/satsumasoftware/timetable/ui/AssignmentsActivity.java
@@ -1,14 +1,29 @@
 package com.satsumasoftware.timetable.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.satsumasoftware.timetable.R;
+import com.satsumasoftware.timetable.db.util.AssignmentsUtils;
+import com.satsumasoftware.timetable.framework.Assignment;
+import com.satsumasoftware.timetable.ui.adapter.AssignmentsAdapter;
+
+import java.util.ArrayList;
 
 public class AssignmentsActivity extends BaseActivity {
+
+    protected static final int REQUEST_CODE_ASSIGNMENT_DETAIL = 1;
+
+    private ArrayList<Assignment> mAssignments;
+    private AssignmentsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +33,49 @@ public class AssignmentsActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mAssignments = AssignmentsUtils.getAssignments(this);
+
+        mAdapter = new AssignmentsAdapter(mAssignments);
+        mAdapter.setOnEntryClickListener(new AssignmentsAdapter.OnEntryClickListener() {
+            @Override
+            public void onEntryClick(View view, int position) {
+                Intent intent = new Intent(AssignmentsActivity.this, AssignmentDetailActivity.class);
+                intent.putExtra(AssignmentDetailActivity.EXTRA_ASSIGNMENT, mAssignments.get(position));
+                startActivityForResult(intent, REQUEST_CODE_ASSIGNMENT_DETAIL);
+            }
+        });
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(mAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AssignmentsActivity.this, AssignmentDetailActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_ASSIGNMENT_DETAIL);
+            }
+        });
+
+    }
+
+    private void refreshList() {
+        mAssignments.clear();
+        mAssignments.addAll(AssignmentsUtils.getAssignments(this));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_ASSIGNMENT_DETAIL) {
+            if (resultCode == Activity.RESULT_OK) {
+                refreshList();
+            }
+        }
     }
 
     @Override
