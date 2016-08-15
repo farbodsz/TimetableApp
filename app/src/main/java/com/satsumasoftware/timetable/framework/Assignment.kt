3e0b4ab844b8c29c282e6bc43454901f4e08bfaf
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-15 19:53:02 +0100

    Make Assignment implement Parcelable

--- app/src/main/java/com/satsumasoftware/timetable/framework/Assignment.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Assignment.kt
@@ -1,11 +1,14 @@
 package com.satsumasoftware.timetable.framework
 
+import android.os.Parcel
+import android.os.Parcelable
+
 import android.database.Cursor
 import com.satsumasoftware.timetable.db.AssignmentsSchema
 import org.threeten.bp.LocalDate
 
 class Assignment(val id: Int, val classId: Int, val title: String, val detail: String,
-                 val dueDate: LocalDate, val completionProgress: Int) {
+                 val dueDate: LocalDate, val completionProgress: Int) : Parcelable {
 
     constructor(cursor: Cursor) : this(
             cursor.getInt(cursor.getColumnIndex(AssignmentsSchema._ID)),
@@ -18,4 +21,29 @@ class Assignment(val id: Int, val classId: Int, val title: String, val detail: S
                     cursor.getInt(cursor.getColumnIndex(AssignmentsSchema.COL_DUE_DATE_YEAR))),
             cursor.getInt(cursor.getColumnIndex(AssignmentsSchema.COL_COMPLETION_PROGRESS)))
 
+    constructor(source: Parcel): this(
+            source.readInt(),
+            source.readInt(),
+            source.readString(),
+            source.readString(),
+            source.readSerializable() as LocalDate,
+            source.readInt())
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id)
+        dest?.writeInt(classId)
+        dest?.writeString(title)
+        dest?.writeString(detail)
+        dest?.writeSerializable(dueDate)
+        dest?.writeInt(completionProgress)
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Assignment> = object : Parcelable.Creator<Assignment> {
+            override fun createFromParcel(source: Parcel): Assignment = Assignment(source)
+            override fun newArray(size: Int): Array<Assignment?> = arrayOfNulls(size)
+        }
+    }
 }
