e0de2a3828190cab81afa5dd1d113343f0df494e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-17 16:45:11 +0100

    Create a function to set toolbar and status bar colors

---
+++ app/src/main/java/com/satsumasoftware/timetable/ThemeUtils.kt
@@ -0,0 +1,15 @@
+package com.satsumasoftware.timetable
+
+import android.app.Activity
+import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.Toolbar
+import com.satsumasoftware.timetable.framework.Color
+
+fun setBarColors(color: Color, activity: Activity, toolbar: Toolbar) {
+    toolbar.setBackgroundColor(ContextCompat.getColor(activity, color.getPrimaryColorResId(activity)))
+    if (Build.VERSION.SDK_INT >= 21) {
+        activity.window.statusBarColor =
+                ContextCompat.getColor(activity, color.getPrimaryDarkColorResId(activity))
+    }
+}
