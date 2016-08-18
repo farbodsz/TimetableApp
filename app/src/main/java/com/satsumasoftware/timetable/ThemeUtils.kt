185ebbf9f568f462384b0fdcff4aca121ad5fd3e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-18 14:59:44 +0100

    Create functions for drawable tinting

--- app/src/main/java/com/satsumasoftware/timetable/ThemeUtils.kt
+++ app/src/main/java/com/satsumasoftware/timetable/ThemeUtils.kt
@@ -1,9 +1,18 @@
 package com.satsumasoftware.timetable
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Build
+import android.support.annotation.ColorRes
+import android.support.annotation.DrawableRes
+import android.support.annotation.IdRes
+import android.support.annotation.MenuRes
 import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.satsumasoftware.timetable.framework.Color
 
@@ -14,5 +23,24 @@ fun setBarColors(color: Color, activity: Activity, vararg views: View) {
     if (Build.VERSION.SDK_INT >= 21) {
         activity.window.statusBarColor =
                 ContextCompat.getColor(activity, color.getPrimaryDarkColorResId(activity))
+    }
+}
+
+@JvmOverloads
+fun tintDrawable(context: Context, @DrawableRes drawableRes: Int, @ColorRes colorRes: Int = R.color.mdu_white): Drawable {
+    return tintDrawable(context, ContextCompat.getDrawable(context, drawableRes), colorRes)
+}
+
+@JvmOverloads
+fun tintDrawable(context: Context, d: Drawable, @ColorRes colorRes: Int = R.color.mdu_white): Drawable {
+    val drawable = DrawableCompat.wrap(d)
+    DrawableCompat.setTint(drawable, ContextCompat.getColor(context, colorRes))
+    return drawable
+}
+
+fun tintMenuIcons(context: Context, menu: Menu, vararg @IdRes menuItems: Int) {
+    for (@IdRes menuItem in menuItems) {
+        val icon = menu.findItem(menuItem).icon
+        icon?.let { tintDrawable(context, icon) }
     }
 }
