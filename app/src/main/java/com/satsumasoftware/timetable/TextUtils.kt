f58cd3757e68c168473c0a8247a6e982ccb0198e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-19 18:34:20 +0100

    Do not change characters in a titled text to lowercase

--- app/src/main/java/com/satsumasoftware/timetable/TextUtils.kt
+++ app/src/main/java/com/satsumasoftware/timetable/TextUtils.kt
@@ -8,7 +8,7 @@ fun String.title(): String {
     val words = this.split(" ")
     val builder = StringBuilder()
     for (word in words) {
-        val titledWord = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase()
+        val titledWord = word.substring(0, 1).toUpperCase() + word.substring(1)
         builder.append(titledWord)
                 .append(" ")
     }
