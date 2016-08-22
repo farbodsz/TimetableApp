ecd17656d2637cde2e3233144188b0f17d8b43e0
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-22 22:34:48 +0100

    Add a constructor to Timetable taking a Cursor as its argument

--- app/src/main/java/com/satsumasoftware/timetable/framework/Timetable.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Timetable.kt
@@ -1,5 +1,21 @@
 package com.satsumasoftware.timetable.framework
 
+import android.database.Cursor
+import com.satsumasoftware.timetable.db.TimetablesSchema
 import org.threeten.bp.LocalDate
 
-class Timetable(val id: Int, val name: String, val startDate: LocalDate, val endDate: LocalDate)
+class Timetable(val id: Int, val name: String, val startDate: LocalDate, val endDate: LocalDate) {
+
+    constructor(cursor: Cursor) : this(
+            cursor.getInt(cursor.getColumnIndex(TimetablesSchema._ID)),
+            cursor.getString(cursor.getColumnIndex(TimetablesSchema.COL_NAME)),
+            LocalDate.of(
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_START_DATE_YEAR)),
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_START_DATE_MONTH)),
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_START_DATE_YEAR))),
+            LocalDate.of(
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_END_DATE_YEAR)),
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_END_DATE_MONTH)),
+                    cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_END_DATE_YEAR))))
+
+}
