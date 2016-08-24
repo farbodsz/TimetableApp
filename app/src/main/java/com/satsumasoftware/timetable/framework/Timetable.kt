e226d00ee83e3aa9999dfed3177f920b818ff793
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-24 04:34:32 +0100

    Add Parcelable implementation for Timetable

--- app/src/main/java/com/satsumasoftware/timetable/framework/Timetable.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Timetable.kt
@@ -1,10 +1,13 @@
 package com.satsumasoftware.timetable.framework
 
 import android.database.Cursor
+import android.os.Parcel
+import android.os.Parcelable
 import com.satsumasoftware.timetable.db.TimetablesSchema
 import org.threeten.bp.LocalDate
 
-class Timetable(val id: Int, val name: String, val startDate: LocalDate, val endDate: LocalDate) {
+class Timetable(val id: Int, val name: String, val startDate: LocalDate,
+                val endDate: LocalDate) : Parcelable {
 
     constructor(cursor: Cursor) : this(
             cursor.getInt(cursor.getColumnIndex(TimetablesSchema._ID)),
@@ -18,4 +21,25 @@ class Timetable(val id: Int, val name: String, val startDate: LocalDate, val end
                     cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_END_DATE_MONTH)),
                     cursor.getInt(cursor.getColumnIndex(TimetablesSchema.COL_END_DATE_YEAR))))
 
+    constructor(source: Parcel) : this(
+            source.readInt(),
+            source.readString(),
+            source.readSerializable() as LocalDate,
+            source.readSerializable() as LocalDate)
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(id)
+        dest?.writeString(name)
+        dest?.writeSerializable(startDate)
+        dest?.writeSerializable(endDate)
+    }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Timetable> = object : Parcelable.Creator<Timetable> {
+            override fun createFromParcel(source: Parcel): Timetable = Timetable(source)
+            override fun newArray(size: Int): Array<Timetable?> = arrayOfNulls(size)
+        }
+    }
 }
