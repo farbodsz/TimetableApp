3a0d69468c80e8f6e252838fe172c3ce313e0932
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-24 16:04:42 +0100

    Save the current timetable with PrefUtils

--- app/src/main/java/com/satsumasoftware/timetable/TimetableApplication.kt
+++ app/src/main/java/com/satsumasoftware/timetable/TimetableApplication.kt
@@ -1,17 +1,29 @@
 package com.satsumasoftware.timetable
 
 import android.app.Application
+import android.util.Log
 
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.satsumasoftware.timetable.framework.Timetable
 
 class TimetableApplication : Application() {
 
+    private val LOG_TAG = "TimetableApplication"
+
     var currentTimetable: Timetable? = null
+        set(value) {
+            field = value
+            value?.let {
+                PrefUtils.setCurrentTimetable(this, field!!)
+                Log.i(LOG_TAG, "Switched current timetable to that with id ${field!!.id}")
+            }
+        }
 
     override fun onCreate() {
         super.onCreate()
 
         AndroidThreeTen.init(this)
+
+        currentTimetable = PrefUtils.getCurrentTimetable(this)
     }
 }
