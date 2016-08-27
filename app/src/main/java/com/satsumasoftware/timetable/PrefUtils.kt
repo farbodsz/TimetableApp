705918710413fb0a1a292f0ab2565a28e0ac61d7
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-08-27 22:09:14 +0100

    Add functions to read/put values for pref_display_weeks_as_letters

--- app/src/main/java/com/satsumasoftware/timetable/PrefUtils.kt
+++ app/src/main/java/com/satsumasoftware/timetable/PrefUtils.kt
@@ -26,5 +26,18 @@ class PrefUtils {
             sp.edit().putInt(PREF_CURRENT_TIMETABLE, timetable.id).apply()
         }
 
+
+        const val PREF_DISPLAY_WEEKS_AS_LETTERS = "pref_display_weeks_as_letters"
+
+        @JvmStatic fun displayWeeksAsLetters(context: Context): Boolean {
+            val sp = PreferenceManager.getDefaultSharedPreferences(context)
+            return sp.getBoolean(PREF_DISPLAY_WEEKS_AS_LETTERS, false)
+        }
+
+        @JvmStatic fun setDisplayWeeksAsLetters(context: Context, boolean: Boolean) {
+            val sp = PreferenceManager.getDefaultSharedPreferences(context)
+            sp.edit().putBoolean(PREF_DISPLAY_WEEKS_AS_LETTERS, boolean).apply()
+        }
+
     }
 }
