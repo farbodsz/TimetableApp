34e49aa19ab64dcee701f1efb05d9e7442825e99
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-09-05 19:00:33 +0100

    Make SettingsActivity extend BaseActivity

--- app/src/main/java/com/satsumasoftware/timetable/ui/SettingsActivity.kt
+++ app/src/main/java/com/satsumasoftware/timetable/ui/SettingsActivity.kt
@@ -1,14 +1,13 @@
 package com.satsumasoftware.timetable.ui
 
-import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceFragment
-import android.support.v7.app.AppCompatActivity
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.Toolbar
 import com.satsumasoftware.timetable.R
-import com.satsumasoftware.timetable.util.ThemeUtils
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +16,9 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        toolbar.navigationIcon = ThemeUtils.tintDrawable(this, R.drawable.ic_done_black_24dp)
-        toolbar.setNavigationOnClickListener { actionDone() }
-
         fragmentManager.beginTransaction()
                 .replace(R.id.content, SettingsFragment())
                 .commit()
-    }
-
-    override fun onBackPressed() {
-        actionDone()
-        super.onBackPressed()
-    }
-
-    fun actionDone() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
     }
 
     class SettingsFragment : PreferenceFragment() {
@@ -43,4 +29,21 @@ class SettingsActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun getSelfToolbar(): Toolbar {
+        return findViewById(R.id.toolbar) as Toolbar
+    }
+
+    override fun getSelfDrawerLayout(): DrawerLayout {
+        return findViewById(R.id.drawerLayout) as DrawerLayout
+    }
+
+    override fun getSelfNavDrawerItem(): Int {
+        return BaseActivity.NAVDRAWER_ITEM_SETTINGS
+    }
+
+    override fun getSelfNavigationView(): NavigationView {
+        return findViewById(R.id.navigationView) as NavigationView
+    }
+
 }
