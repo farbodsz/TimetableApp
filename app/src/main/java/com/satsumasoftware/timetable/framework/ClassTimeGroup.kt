bc7b517a73009be605342c156ea8750695a994f5
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-12-20 19:02:50 +0000

    Include documentation for ClassTimeGroup.kt

--- app/src/main/java/com/satsumasoftware/timetable/framework/ClassTimeGroup.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/ClassTimeGroup.kt
@@ -3,6 +3,13 @@ package com.satsumasoftware.timetable.framework
 import org.threeten.bp.LocalTime
 import java.util.*
 
+/**
+ * A data structure merely to group `ClassTime` objects with the same start and end dates as each
+ * other.
+ *
+ * @property startTime The start time all `ClassTime`s in this group will have
+ * @property endTime The end time all `ClassTime`s in this group will have
+ */
 class ClassTimeGroup(val startTime: LocalTime, val endTime: LocalTime) {
 
     val classTimes = ArrayList<ClassTime>()
@@ -15,6 +22,9 @@ class ClassTimeGroup(val startTime: LocalTime, val endTime: LocalTime) {
         classTimes.add(classTime)
     }
 
+    /**
+     * @return if the [classTime] can be added to the group (if it has the same start and end times)
+     */
     fun canAdd(classTime: ClassTime) =
             classTime.startTime.equals(startTime) && classTime.endTime.equals(endTime)
 
