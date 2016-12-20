7f257f67972106be000570fd1c2575074c257452
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-12-20 18:37:56 +0000

    Add documentation for ClassDetail.kt

--- app/src/main/java/com/satsumasoftware/timetable/framework/ClassDetail.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/ClassDetail.kt
@@ -5,6 +5,21 @@ import android.database.Cursor
 import com.satsumasoftware.timetable.db.ClassDetailsSchema
 import com.satsumasoftware.timetable.db.TimetableDbHelper
 
+/**
+ * An object to represent one set of details of a class.
+ *
+ * These details include the location of the class and the teacher's name.
+ *
+ * The reason we represent the data in this way is because a class can take place in more than one
+ * location, or be taught by more than one different teacher. Therefore, a `Class` can be linked to
+ * multiple `ClassDetail`s; this is why our `ClassDetail` contains a `classId` property.
+ *
+ * @property id An integer used to identify this class detail
+ * @property classId The identifier of the associated class
+ * @property room An optional name of the room where the class takes place
+ * @property building An optional name of the building where the class takes place
+ * @property teacher The name of the teacher for the class (also optional)
+ */
 class ClassDetail(val id: Int, val classId: Int, val room: String, val building: String,
                   val teacher: String) {
 
