e1d37fb67cb620d26ae2997229711f5a1e176afc
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2016-12-21 17:52:27 +0000

    Add documentation for ClassDetailsSchema

--- app/src/main/java/com/satsumasoftware/timetable/db/ClassDetailsSchema.java
+++ app/src/main/java/com/satsumasoftware/timetable/db/ClassDetailsSchema.java
@@ -4,6 +4,12 @@ import android.provider.BaseColumns;
 
 import com.satsumasoftware.timetable.db.util.SchemaUtilsKt;
 
+/**
+ * The schema for the 'class_details' table, containing constants for the column names and an
+ * SQLite create statement.
+ *
+ * @see com.satsumasoftware.timetable.framework.ClassDetail
+ */
 public final class ClassDetailsSchema implements BaseColumns {
 
     public static final String TABLE_NAME = "class_details";
@@ -12,6 +18,11 @@ public final class ClassDetailsSchema implements BaseColumns {
     public static final String COL_BUILDING = "building";
     public static final String COL_TEACHER = "teacher";
 
+    /**
+     * An SQLite statement which creates the 'class_details' table upon execution.
+     *
+     * @see TimetableDbHelper
+     */
     static final String SQL_CREATE = "CREATE TABLE " + TABLE_NAME + "( " +
             _ID + SchemaUtilsKt.INTEGER_TYPE + SchemaUtilsKt.PRIMARY_KEY_AUTOINCREMENT + SchemaUtilsKt.COMMA_SEP +
             COL_CLASS_ID + SchemaUtilsKt.INTEGER_TYPE + SchemaUtilsKt.COMMA_SEP +
