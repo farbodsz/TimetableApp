11af3f6734a1cfa2c9cfff1723d4fd698111aa0e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-01-10 20:40:20 +0000

    Create LicensesActivity to display some licenses

---
+++ app/src/main/java/com/satsumasoftware/timetable/ui/LicensesActivity.kt
@@ -0,0 +1,16 @@
+package com.satsumasoftware.timetable.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.satsumasoftware.timetable.R
+
+class LicensesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_licenses)
+
+        // TODO
+    }
+
+}
