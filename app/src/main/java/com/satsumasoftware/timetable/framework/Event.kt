8a4ff3655b793f031b5dff9938de6e63e91b2665
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-01-27 17:01:21 +0000

    Add a static factory to construct Event with a cursor

--- app/src/main/java/com/satsumasoftware/timetable/framework/Event.kt
+++ app/src/main/java/com/satsumasoftware/timetable/framework/Event.kt
@@ -1,8 +1,12 @@
 package com.satsumasoftware.timetable.framework
 
+import android.database.Cursor
 import android.os.Parcel
 import android.os.Parcelable
+import com.satsumasoftware.timetable.db.EventsSchema
+import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
 
 /**
  * Represents an event that is not part of the usual schedule of the user.
@@ -19,6 +23,40 @@ class Event(val id: Int, val timetableId: Int, val title: String, val detail: St
             val startTime: LocalDateTime, val endTime: LocalDateTime) : Parcelable {
 
     companion object {
+
+        /**
+         * Constructs an Event using column values from the cursor provided
+         *
+         * @param cursor a query of the events table
+         * @see [EventsSchema]
+         */
+        @JvmStatic
+        fun from(cursor: Cursor): Event {
+            val startDate = LocalDate.of(
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_START_DATE_YEAR)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_START_DATE_MONTH)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_START_DATE_DAY_OF_MONTH)))
+            val startTime = LocalTime.of(
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_START_TIME_HRS)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_START_TIME_MINS)))
+
+            val endDate = LocalDate.of(
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_END_DATE_YEAR)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_END_DATE_MONTH)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_END_DATE_DAY_OF_MONTH)))
+            val endTime = LocalTime.of(
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_END_TIME_HRS)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_END_TIME_MINS)))
+
+            return Event(
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema._ID)),
+                    cursor.getInt(cursor.getColumnIndex(EventsSchema.COL_TIMETABLE_ID)),
+                    cursor.getString(cursor.getColumnIndex(EventsSchema.COL_TITLE)),
+                    cursor.getString(cursor.getColumnIndex(EventsSchema.COL_DETAIL)),
+                    LocalDateTime.of(startDate, startTime),
+                    LocalDateTime.of(endDate, endTime))
+        }
+
         @JvmField val CREATOR: Parcelable.Creator<Event> = object : Parcelable.Creator<Event> {
             override fun createFromParcel(source: Parcel): Event = Event(source)
             override fun newArray(size: Int): Array<Event?> = arrayOfNulls(size)
