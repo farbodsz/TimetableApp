2e1c5088f76ccf33279f7450e66c404fe088e686
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-02-02 17:16:40 +0000

    Rewrite db schema classes in Kotlin

---
+++ app/src/main/java/com/satsumasoftware/timetable/db/TimetablesSchema.kt
@@ -0,0 +1,44 @@
+package com.satsumasoftware.timetable.db
+
+import android.provider.BaseColumns
+import com.satsumasoftware.timetable.db.util.COMMA_SEP
+import com.satsumasoftware.timetable.db.util.INTEGER_TYPE
+import com.satsumasoftware.timetable.db.util.PRIMARY_KEY_AUTOINCREMENT
+import com.satsumasoftware.timetable.db.util.TEXT_TYPE
+
+/**
+ * The schema for the 'timetables' table, containing constants for the column names and an SQLite
+ * create statement.
+
+ * @see com.satsumasoftware.timetable.framework.Timetable
+ */
+object TimetablesSchema : BaseColumns {
+
+    const val TABLE_NAME = "timetables"
+    const val COL_NAME = "name"
+    const val COL_START_DATE_DAY_OF_MONTH = "start_date_day_of_month"
+    const val COL_START_DATE_MONTH = "start_date_month"
+    const val COL_START_DATE_YEAR = "start_date_year"
+    const val COL_END_DATE_DAY_OF_MONTH = "end_date_day_of_month"
+    const val COL_END_DATE_MONTH = "end_date_month"
+    const val COL_END_DATE_YEAR = "end_date_year"
+    const val COL_WEEK_ROTATIONS = "week_rotations"
+
+    /**
+     * An SQLite statement which creates the 'timetables' table upon execution.
+
+     * @see TimetableDbHelper
+     */
+    internal const val SQL_CREATE = "CREATE TABLE " + TABLE_NAME + "( " +
+            BaseColumns._ID + INTEGER_TYPE + PRIMARY_KEY_AUTOINCREMENT + COMMA_SEP +
+            COL_NAME + TEXT_TYPE + COMMA_SEP +
+            COL_START_DATE_DAY_OF_MONTH + INTEGER_TYPE + COMMA_SEP +
+            COL_START_DATE_MONTH + INTEGER_TYPE + COMMA_SEP +
+            COL_START_DATE_YEAR + INTEGER_TYPE + COMMA_SEP +
+            COL_END_DATE_DAY_OF_MONTH + INTEGER_TYPE + COMMA_SEP +
+            COL_END_DATE_MONTH + INTEGER_TYPE + COMMA_SEP +
+            COL_END_DATE_YEAR + INTEGER_TYPE + COMMA_SEP +
+            COL_WEEK_ROTATIONS + INTEGER_TYPE +
+            " )"
+
+}
