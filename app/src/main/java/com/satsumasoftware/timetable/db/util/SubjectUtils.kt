e00de88b27df46f3d6b462707a66bfbdf62edfd9
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-02-02 16:44:50 +0000

    Make util classes objects, removing redundant companion objects

--- app/src/main/java/com/satsumasoftware/timetable/db/util/SubjectUtils.kt
+++ app/src/main/java/com/satsumasoftware/timetable/db/util/SubjectUtils.kt
@@ -10,99 +10,96 @@ import com.satsumasoftware.timetable.db.TimetableDbHelper
 import com.satsumasoftware.timetable.framework.Subject
 import java.util.*
 
-class SubjectUtils {
+object SubjectUtils {
 
-    companion object {
+    private const val LOG_TAG = "SubjectUtils"
 
-        private const val LOG_TAG = "SubjectUtils"
+    @JvmStatic
+    fun getSubjects(activity: Activity): ArrayList<Subject> {
+        val subjects = ArrayList<Subject>()
 
-        @JvmStatic
-        fun getSubjects(activity: Activity): ArrayList<Subject> {
-            val subjects = ArrayList<Subject>()
+        val timetable = (activity.application as TimetableApplication).currentTimetable!!
 
-            val timetable = (activity.application as TimetableApplication).currentTimetable!!
-
-            val dbHelper = TimetableDbHelper.getInstance(activity)
-            val cursor = dbHelper.readableDatabase.query(
-                    SubjectsSchema.TABLE_NAME,
-                    null,
-                    "${SubjectsSchema.COL_TIMETABLE_ID}=?",
-                    arrayOf(timetable.id.toString()),
-                    null, null, null)
-            cursor.moveToFirst()
-            while (!cursor.isAfterLast) {
-                subjects.add(Subject.from(cursor))
-                cursor.moveToNext()
-            }
-            cursor.close()
-            return subjects
+        val dbHelper = TimetableDbHelper.getInstance(activity)
+        val cursor = dbHelper.readableDatabase.query(
+                SubjectsSchema.TABLE_NAME,
+                null,
+                "${SubjectsSchema.COL_TIMETABLE_ID}=?",
+                arrayOf(timetable.id.toString()),
+                null, null, null)
+        cursor.moveToFirst()
+        while (!cursor.isAfterLast) {
+            subjects.add(Subject.from(cursor))
+            cursor.moveToNext()
         }
-
-        @JvmStatic
-        fun addSubject(context: Context, subject: Subject) {
-            val values = ContentValues()
-            with(values) {
-                put(SubjectsSchema._ID, subject.id)
-                put(SubjectsSchema.COL_TIMETABLE_ID, subject.timetableId)
-                put(SubjectsSchema.COL_NAME, subject.name)
-                put(SubjectsSchema.COL_ABBREVIATION, subject.abbreviation)
-                put(SubjectsSchema.COL_COLOR_ID, subject.colorId)
-            }
-
-            val db = TimetableDbHelper.getInstance(context).writableDatabase
-            db.insert(SubjectsSchema.TABLE_NAME, null, values)
-            Log.i(LOG_TAG, "Added Subject with id ${subject.id}")
-        }
-
-        private fun deleteSubject(context: Context, subjectId: Int) {
-            val db = TimetableDbHelper.getInstance(context).writableDatabase
-            db.delete(SubjectsSchema.TABLE_NAME,
-                    "${SubjectsSchema._ID}=?",
-                    arrayOf(subjectId.toString()))
-            Log.i(LOG_TAG, "Deleted Subject with id $subjectId")
-        }
-
-        @JvmStatic
-        fun completelyDeleteSubject(context: Context, subject: Subject) {
-            Log.i(LOG_TAG, "Deleting everything related to Subject with id ${subject.id}")
-
-            deleteSubject(context, subject.id)
-
-            for (cls in ClassUtils.getClassesForSubject(context, subject.id)) {
-                ClassUtils.completelyDeleteClass(context, cls)
-            }
-
-            for (exam in ExamUtils.getExamsForSubject(context, subject.id)) {
-                ExamUtils.deleteExam(context, exam.id)
-            }
-        }
-
-        @JvmStatic
-        fun replaceSubject(context: Context, oldSubjectId: Int, newSubject: Subject) {
-            Log.i(LOG_TAG, "Replacing Subject...")
-            deleteSubject(context, oldSubjectId)
-            addSubject(context, newSubject)
-        }
-
-        @JvmStatic
-        fun getHighestSubjectId(context: Context): Int {
-            val db = TimetableDbHelper.getInstance(context).readableDatabase
-            val cursor = db.query(
-                    SubjectsSchema.TABLE_NAME,
-                    arrayOf(SubjectsSchema._ID),
-                    null,
-                    null,
-                    null,
-                    null,
-                    "${SubjectsSchema._ID} DESC")
-            if (cursor.count == 0) {
-                return 0
-            }
-            cursor.moveToFirst()
-            val highestId = cursor.getInt(cursor.getColumnIndex(SubjectsSchema._ID))
-            cursor.close()
-            return highestId
-        }
-
+        cursor.close()
+        return subjects
     }
+
+    @JvmStatic
+    fun addSubject(context: Context, subject: Subject) {
+        val values = ContentValues()
+        with(values) {
+            put(SubjectsSchema._ID, subject.id)
+            put(SubjectsSchema.COL_TIMETABLE_ID, subject.timetableId)
+            put(SubjectsSchema.COL_NAME, subject.name)
+            put(SubjectsSchema.COL_ABBREVIATION, subject.abbreviation)
+            put(SubjectsSchema.COL_COLOR_ID, subject.colorId)
+        }
+
+        val db = TimetableDbHelper.getInstance(context).writableDatabase
+        db.insert(SubjectsSchema.TABLE_NAME, null, values)
+        Log.i(LOG_TAG, "Added Subject with id ${subject.id}")
+    }
+
+    private fun deleteSubject(context: Context, subjectId: Int) {
+        val db = TimetableDbHelper.getInstance(context).writableDatabase
+        db.delete(SubjectsSchema.TABLE_NAME,
+                "${SubjectsSchema._ID}=?",
+                arrayOf(subjectId.toString()))
+        Log.i(LOG_TAG, "Deleted Subject with id $subjectId")
+    }
+
+    @JvmStatic
+    fun completelyDeleteSubject(context: Context, subject: Subject) {
+        Log.i(LOG_TAG, "Deleting everything related to Subject with id ${subject.id}")
+
+        deleteSubject(context, subject.id)
+
+        for (cls in ClassUtils.getClassesForSubject(context, subject.id)) {
+            ClassUtils.completelyDeleteClass(context, cls)
+        }
+
+        for (exam in ExamUtils.getExamsForSubject(context, subject.id)) {
+            ExamUtils.deleteExam(context, exam.id)
+        }
+    }
+
+    @JvmStatic
+    fun replaceSubject(context: Context, oldSubjectId: Int, newSubject: Subject) {
+        Log.i(LOG_TAG, "Replacing Subject...")
+        deleteSubject(context, oldSubjectId)
+        addSubject(context, newSubject)
+    }
+
+    @JvmStatic
+    fun getHighestSubjectId(context: Context): Int {
+        val db = TimetableDbHelper.getInstance(context).readableDatabase
+        val cursor = db.query(
+                SubjectsSchema.TABLE_NAME,
+                arrayOf(SubjectsSchema._ID),
+                null,
+                null,
+                null,
+                null,
+                "${SubjectsSchema._ID} DESC")
+        if (cursor.count == 0) {
+            return 0
+        }
+        cursor.moveToFirst()
+        val highestId = cursor.getInt(cursor.getColumnIndex(SubjectsSchema._ID))
+        cursor.close()
+        return highestId
+    }
+
 }
