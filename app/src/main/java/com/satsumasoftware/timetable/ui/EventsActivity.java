1e8a201cb3dd60afbb8fd562d3bbc61df1f30ae5
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-02-20 22:15:34 +0000

    Use ItemListActivity as a superclass of EventsActivity

--- app/src/main/java/com/satsumasoftware/timetable/ui/EventsActivity.java
+++ app/src/main/java/com/satsumasoftware/timetable/ui/EventsActivity.java
@@ -3,21 +3,17 @@ package com.satsumasoftware.timetable.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.satsumasoftware.timetable.R;
+import com.satsumasoftware.timetable.db.handler.EventHandler;
+import com.satsumasoftware.timetable.db.handler.TimetableItemHandler;
 import com.satsumasoftware.timetable.db.util.EventUtils;
 import com.satsumasoftware.timetable.framework.Event;
 import com.satsumasoftware.timetable.ui.adapter.EventsAdapter;
@@ -41,58 +37,39 @@ import java.util.Comparator;
  *
  * @see Event
  */
-public class EventsActivity extends BaseActivity {
+public class EventsActivity extends ItemListActivity<Event> {
 
     private static final int REQUEST_CODE_EVENT_EDIT = 1;
 
     private ArrayList<String> mHeaders;
-    private ArrayList<Event> mEvents;
-
-    private EventsAdapter mAdapter;
-
-    private RecyclerView mRecyclerView;
-    private FrameLayout mPlaceholderLayout;
 
     private boolean mShowPast;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_content_list);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        setupLayout();
+    TimetableItemHandler<Event> instantiateDataHandler() {
+        return new EventHandler(this);
     }
 
-    private void setupLayout() {
-        setupList();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(EventsActivity.this, EventEditActivity.class);
-                startActivityForResult(intent, REQUEST_CODE_EVENT_EDIT);
-            }
-        });
-
-        mPlaceholderLayout = (FrameLayout) findViewById(R.id.placeholder);
-        refreshPlaceholderStatus();
+    @Override
+    void onFabButtonClick() {
+        Intent intent = new Intent(EventsActivity.this, EventEditActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_EVENT_EDIT);
     }
 
-    private void setupList() {
+    @Override
+    void setupList() {
         mHeaders = new ArrayList<>();
-        mEvents = EventUtils.getEvents(this, getApplication());
-        sortList();
+        super.setupList();
+    }
 
-        mAdapter = new EventsAdapter(this, mHeaders, mEvents);
-        mAdapter.setOnEntryClickListener(new EventsAdapter.OnEntryClickListener() {
+    @Override
+    RecyclerView.Adapter setupAdapter() {
+        EventsAdapter adapter = new EventsAdapter(this, mHeaders, mItems);
+        adapter.setOnEntryClickListener(new EventsAdapter.OnEntryClickListener() {
             @Override
             public void onEntryClick(View view, int position) {
                 Intent intent = new Intent(EventsActivity.this, EventEditActivity.class);
-                intent.putExtra(EventEditActivity.EXTRA_EVENT, mEvents.get(position));
+                intent.putExtra(EventEditActivity.EXTRA_EVENT, mItems.get(position));
 
                 Bundle bundle = null;
                 if (UiUtils.isApi21()) {
@@ -109,16 +86,12 @@ public class EventsActivity extends BaseActivity {
             }
         });
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.addItemDecoration(
-                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setAdapter(mAdapter);
+        return adapter;
     }
 
-    private void sortList() {
-        Collections.sort(mEvents, new Comparator<Event>() {
+    @Override
+    void sortList() {
+        Collections.sort(mItems, new Comparator<Event>() {
             @Override
             public int compare(Event e1, Event e2) {
                 LocalDateTime dateTime1 = e1.getStartTime();
@@ -136,8 +109,8 @@ public class EventsActivity extends BaseActivity {
 
         int currentTimePeriod = -1;
 
-        for (int i = 0; i < mEvents.size(); i++) {
-            Event event = mEvents.get(i);
+        for (int i = 0; i < mItems.size(); i++) {
+            Event event = mItems.get(i);
 
             LocalDateTime eventDate = event.getStartTime();
             int timePeriodId;
@@ -179,39 +152,31 @@ public class EventsActivity extends BaseActivity {
         mHeaders.clear();
         mHeaders.addAll(headers);
 
-        mEvents.clear();
-        mEvents.addAll(events);
+        mItems.clear();
+        mItems.addAll(events);
     }
 
-    private void refreshList() {
-        mEvents.clear();
-        mEvents.addAll(EventUtils.getEvents(this, getApplication()));
+    @Override
+    void refreshList() {
+        mItems.clear();
+        mItems.addAll(EventUtils.getEvents(this, getApplication()));
         sortList();
         mAdapter.notifyDataSetChanged();
         refreshPlaceholderStatus();
     }
 
-    private void refreshPlaceholderStatus() {
-        if (mEvents.isEmpty()) {
-            mRecyclerView.setVisibility(View.GONE);
-            mPlaceholderLayout.setVisibility(View.VISIBLE);
+    @Override
+    View getPlaceholderView() {
+        int stringRes = mShowPast ? R.string.placeholder_events_past :
+                R.string.placeholder_events;
 
-            int stringRes = mShowPast ? R.string.placeholder_events_past :
-                    R.string.placeholder_events;
-
-            mPlaceholderLayout.removeAllViews();
-            mPlaceholderLayout.addView(UiUtils.makePlaceholderView(this,
-                    R.drawable.ic_event_black_24dp,
-                    stringRes,
-                    R.color.mdu_blue_400,
-                    R.color.mdu_white,
-                    R.color.mdu_white,
-                    true));
-
-        } else {
-            mRecyclerView.setVisibility(View.VISIBLE);
-            mPlaceholderLayout.setVisibility(View.GONE);
-        }
+        return UiUtils.makePlaceholderView(this,
+                R.drawable.ic_event_black_24dp,
+                stringRes,
+                R.color.mdu_blue_400,
+                R.color.mdu_white,
+                R.color.mdu_white,
+                true);
     }
 
     @Override
@@ -253,23 +218,8 @@ public class EventsActivity extends BaseActivity {
     }
 
     @Override
-    protected Toolbar getSelfToolbar() {
-        return (Toolbar) findViewById(R.id.toolbar);
-    }
-
-    @Override
-    protected DrawerLayout getSelfDrawerLayout() {
-        return (DrawerLayout) findViewById(R.id.drawerLayout);
-    }
-
-    @Override
     protected int getSelfNavDrawerItem() {
         return NAVDRAWER_ITEM_EVENTS;
-    }
-
-    @Override
-    protected NavigationView getSelfNavigationView() {
-        return (NavigationView) findViewById(R.id.navigationView);
     }
 
 }
