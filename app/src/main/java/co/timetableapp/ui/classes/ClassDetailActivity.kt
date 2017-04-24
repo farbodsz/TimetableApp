0b59d1a95cc9d35c0f3fbd8ed088ecd30c61210a
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2017-04-24 17:17:17 +0100

    Redesign the class detail page

--- app/src/main/java/co/timetableapp/ui/classes/ClassDetailActivity.kt
+++ app/src/main/java/co/timetableapp/ui/classes/ClassDetailActivity.kt
@@ -2,21 +2,29 @@ package co.timetableapp.ui.classes
 
 import android.app.Activity
 import android.content.Intent
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.ActivityCompat
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import co.timetableapp.R
-import co.timetableapp.data.handler.ClassDetailHandler
-import co.timetableapp.data.handler.ClassHandler
-import co.timetableapp.data.handler.ClassTimeHandler
+import co.timetableapp.TimetableApplication
+import co.timetableapp.data.handler.*
+import co.timetableapp.data.query.Filters
+import co.timetableapp.data.query.Query
+import co.timetableapp.data.schema.AssignmentsSchema
+import co.timetableapp.data.schema.ExamsSchema
 import co.timetableapp.model.Class
 import co.timetableapp.model.ClassTime
 import co.timetableapp.model.Color
 import co.timetableapp.model.Subject
+import co.timetableapp.ui.agenda.AgendaActivity
 import co.timetableapp.ui.base.ItemDetailActivity
 import co.timetableapp.ui.base.ItemEditActivity
+import co.timetableapp.ui.components.CardOfItems
 import co.timetableapp.util.UiUtils
+import org.threeten.bp.format.DateTimeFormatter
 
 /**
  * Shows the details of a class.
@@ -38,9 +46,32 @@ class ClassDetailActivity : ItemDetailActivity<Class>() {
     }
 
     override fun setupLayout() {
-        val subject = Subject.create(this, mItem!!.subjectId)!!
-        setupToolbar(subject)
+        setupToolbar()
+        setupClassDetailCard()
+        setupRelatedItemCards()
+    }
 
+    private fun setupToolbar() {
+        val subject = Subject.create(this, mItem!!.subjectId)!!
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        toolbar.navigationIcon = UiUtils.tintDrawable(this, R.drawable.ic_arrow_back_black_24dp)
+        toolbar.setNavigationOnClickListener { saveEditsAndClose() }
+
+        val ctl = findViewById(R.id.collapsingToolbarLayout) as CollapsingToolbarLayout
+        ctl.title = mItem!!.makeName(subject)
+
+        val color = Color(subject.colorId)
+        UiUtils.setBarColors(color, this, toolbar)
+    }
+
+    /**
+     * Adds content to a card displaying details about the class (such as locations, teachers, and
+     * the times of the class).
+     */
+    private fun setupClassDetailCard() {
         val locationBuilder = StringBuilder()
         val teacherBuilder = StringBuilder()
         val allClassTimes = ArrayList<ClassTime>()
@@ -62,23 +93,10 @@ class ClassDetailActivity : ItemDetailActivity<Class>() {
             allClassTimes.addAll(ClassTimeHandler.getClassTimesForDetail(this, classDetail.id))
         }
 
-        displayData(
+        setClassDetailTexts(
                 locationBuilder.toString().removeSuffix("\n"),
                 teacherBuilder.toString().removeSuffix("\n"),
                 produceClassTimesText(allClassTimes))
-    }
-
-    private fun setupToolbar(subject: Subject) {
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-
-        toolbar.navigationIcon = UiUtils.tintDrawable(this, R.drawable.ic_arrow_back_black_24dp)
-        toolbar.setNavigationOnClickListener { saveEditsAndClose() }
-
-        supportActionBar!!.title = mItem!!.makeName(subject)
-
-        val color = Color(subject.colorId)
-        UiUtils.setBarColors(color, this, toolbar)
     }
 
     /**
@@ -112,14 +130,14 @@ class ClassDetailActivity : ItemDetailActivity<Class>() {
     }
 
     /**
-     * Sets the text on the TextViews for the class data.
+     * Sets the text on the TextViews for the class detail data.
      * Appropriate parts of the UI will not be displayed (i.e. when there is no text for an item).
      *
      * @param locations text to be displayed for the class locations
      * @param teachers text to be displayed for the teachers of the class
      * @param classTimes text to be displayed for the class times
      */
-    private fun displayData(locations: String, teachers: String, classTimes: String) {
+    private fun setClassDetailTexts(locations: String, teachers: String, classTimes: String) {
         val locationVisibility = if (locations.isEmpty()) View.GONE else View.VISIBLE
         findViewById(R.id.viewGroup_location).visibility = locationVisibility
         findViewById(R.id.divider_location).visibility = locationVisibility
@@ -137,6 +155,83 @@ class ClassDetailActivity : ItemDetailActivity<Class>() {
         // No need to check if it's empty - all class details must have a class time
         val textViewTimes = findViewById(R.id.textView_times) as TextView
         textViewTimes.text = classTimes
+    }
+
+    /**
+     * Sets up the UI for cards displaying items related to this class, such as assignments and
+     * exams for this class.
+     */
+    private fun setupRelatedItemCards() {
+        val cardContainer = findViewById(R.id.card_container) as LinearLayout
+
+        val assignmentsCard = CardOfItems.Builder(this, cardContainer)
+                .setTitle(R.string.title_assignments)
+                .setItems(createAssignmentItems())
+                .setButtonProperties(R.string.view_all, View.OnClickListener {
+                    startActivity(Intent(this@ClassDetailActivity, AgendaActivity::class.java))
+                })
+                .build()
+                .view
+        cardContainer.addView(assignmentsCard)
+
+        val examsCard = CardOfItems.Builder(this, cardContainer)
+                .setTitle(R.string.title_exams)
+                .setItems(createExamItems())
+                .setButtonProperties(R.string.view_all, View.OnClickListener {
+                    startActivity(Intent(this@ClassDetailActivity, AgendaActivity::class.java))
+                })
+                .build()
+                .view
+        cardContainer.addView(examsCard)
+    }
+
+    private fun createAssignmentItems(): ArrayList<CardOfItems.CardItem> {
+        val timetableId = (application as TimetableApplication).currentTimetable!!.id
+
+        // Get assignments for this class
+        val query = Query.Builder()
+                .addFilter(Filters.equal(AssignmentsSchema.COL_TIMETABLE_ID, timetableId.toString()))
+                .addFilter(Filters.equal(AssignmentsSchema.COL_CLASS_ID, mItem!!.id.toString()))
+                .build()
+
+        // Create items
+        val items = ArrayList<CardOfItems.CardItem>()
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM uuuu")
+        AssignmentHandler(this).getAllItems(query).forEach {
+            if (it.isUpcoming() || it.isOverdue()) {
+                items.add(CardOfItems.CardItem(
+                        it.title,
+                        it.dueDate.format(formatter),
+                        null))
+            }
+        }
+
+        return items
+    }
+
+    private fun createExamItems(): ArrayList<CardOfItems.CardItem> {
+        val timetableId = (application as TimetableApplication).currentTimetable!!.id
+
+        // Get exams for this class (actually for the subject of the class)
+        val query = Query.Builder()
+                .addFilter(Filters.equal(ExamsSchema.COL_TIMETABLE_ID, timetableId.toString()))
+                .addFilter(Filters.equal(ExamsSchema.COL_SUBJECT_ID, mItem!!.subjectId.toString()))
+                .build()
+
+        // Create items
+        val items = ArrayList<CardOfItems.CardItem>()
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM uuuu")
+        val subject = Subject.create(this, mItem!!.subjectId)!!
+        ExamHandler(this).getAllItems(query).forEach {
+            if (it.isUpcoming()) {
+                items.add(CardOfItems.CardItem(
+                        it.makeName(subject),
+                        it.date.format(formatter),
+                        null))
+            }
+        }
+
+        return items
     }
 
     override fun onMenuEditClick() {
---
+++ app/src/main/java/co/timetableapp/ui/components/CardOfItems.kt
@@ -0,0 +1,138 @@
+package co.timetableapp.ui.components
+
+import android.content.Context
+import android.support.annotation.StringRes
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import co.timetableapp.R
+import co.timetableapp.model.Color
+import co.timetableapp.ui.components.CardOfItems.Builder
+import de.hdodenhof.circleimageview.CircleImageView
+
+/**
+ * This class can be used to create a View of a card displaying a list of sub-items inside.
+ *
+ * Specifically, the layout will include a title at the top of the card, then a list of items below
+ * it. At the bottom of the card, there is a button to view.
+ *
+ * An instance of this class must be constructed using the [Builder].
+ */
+@Suppress("unused")
+class CardOfItems private constructor(val view: View) {
+
+    /**
+     * Represents a sub-item that would be displayed on this card component.
+     */
+    class CardItem(
+            val title: String,
+            val subtitle: String,
+            val color: Color?
+    )
+
+    /**
+     * A builder to create a [CardOfItems].
+     *
+     * If a title is not specified (using [setTitle]), it will not be shown on the card.
+     * Similarly, if button properties are not specified with [setButtonProperties], then a button
+     * will not be shown on the card.
+     */
+    class Builder(private val mContext: Context, private val mRoot: ViewGroup) {
+
+        private var mTitle: String? = null
+        private var mItems: ArrayList<CardItem>? = null
+        private var mButtonText: String? = null
+        private var mButtonClickListener: View.OnClickListener? = null
+
+        fun setTitle(title: String): Builder {
+            mTitle = title
+            return this
+        }
+
+        fun setTitle(@StringRes titleRes: Int): Builder {
+            mTitle = mContext.getString(titleRes)
+            return this
+        }
+
+        fun setItems(items: ArrayList<CardItem>): Builder {
+            mItems = items
+            return this
+        }
+
+        fun setButtonProperties(buttonText: String,
+                                onClickListener: View.OnClickListener): Builder {
+            mButtonText = buttonText
+            mButtonClickListener = onClickListener
+            return this
+        }
+
+        fun setButtonProperties(@StringRes buttonTextRes: Int,
+                                onClickListener: View.OnClickListener): Builder {
+            setButtonProperties(mContext.getString(buttonTextRes), onClickListener)
+            return this
+        }
+
+        fun build(): CardOfItems {
+            val inflater = LayoutInflater.from(mContext)
+
+            val card = inflater.inflate(R.layout.item_card_of_items, mRoot, false)
+
+            with(card) {
+                val titleView = findViewById(R.id.title) as TextView
+                if (mTitle == null) {
+                    titleView.visibility = View.GONE
+                } else {
+                    titleView.text = mTitle
+                }
+
+                if (mItems != null && mItems!!.isNotEmpty()) {
+                    val container = findViewById(R.id.container) as LinearLayout
+
+                    container.removeAllViews() // by default contains a placeholder text
+                    populateCardContainer(container, mItems!!)
+                }
+
+                val button = findViewById(R.id.button) as Button
+                if (mButtonText == null) {
+                    button.visibility = View.GONE
+                } else {
+                    button.text = mButtonText
+                    button.setOnClickListener(mButtonClickListener)
+                }
+            }
+
+            return CardOfItems(card)
+        }
+
+        /**
+         * Populates the container with the list of items to display on the card
+         */
+        private fun populateCardContainer(container: LinearLayout, items: ArrayList<CardItem>) {
+            items.forEach {
+                val itemView = LayoutInflater.from(mContext)
+                        .inflate(R.layout.item_condensed, container, false)
+
+                with(itemView) {
+                    (findViewById(R.id.title) as TextView).text = it.title
+                    (findViewById(R.id.subtitle) as TextView).text = it.subtitle
+
+                    val colorView = findViewById(R.id.imageView) as CircleImageView
+                    if (it.color == null) {
+                        colorView.visibility = View.GONE
+                    } else {
+                        colorView.setBackgroundColor(ContextCompat.getColor(
+                                mContext,
+                                it.color.getPrimaryColorResId(mContext)))
+                    }
+                }
+
+                container.addView(itemView)
+            }
+        }
+    }
+
+}
